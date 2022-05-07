8507ecbdf3295cccb5d54fcb11a9c24980f0edca
DanK1368 <danielkoett@gmail.com>
2022-05-07 09:49:40 +0200

    initial commit of project

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,10 @@
+import "./Header.scss";
+
+function Header() {
+  return (
+    <header className="header">
+      <img src="./assets/logo-dark.svg" alt="" />
+    </header>
+  );
+}
+export default Header;
